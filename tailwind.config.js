7e21fd3c9bfefcf3893a98496efa231d47610e8a
jacob-s-codes <jacobbshaul@gmail.com>
2025-01-19 21:13:46 -0800

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content:["*.html", "*/index.html", "*/**/index.html"],
+  theme: {
+    extend: {},
+    
+  },
+  plugins: [
+  ],
+}
+
