d2e6122e4170cd14194a892bad2ed10303cb1a4b
jacob-s-codes <jacobbshaul@gmail.com>
2025-01-20 20:19:57 -0800

    updated about page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     
     extend: {
       colors: {
-        primary: '#586994',
+        deepblue: '#586994',
+        lightblue: '#7D859C',
+        lightgray: '#A2ABAB',
+        darkgreen: '#B4C4AE',
+        yellowcolor: '#E5E8B6',
       },
     },
     
